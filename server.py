4eb83a1eac0a14b9861eb02f7220af457c6c3cd1
bilalarsln <bilal.arslan@ogr.gidatarim.edu.tr>
2024-06-26 23:40:46 +0300

    dosya yükleme hatası giderildi

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'AIPotato'
+app.config['MYSQL_DB'] = 'aipotato'
 
 mysql = MySQL(app)
 
